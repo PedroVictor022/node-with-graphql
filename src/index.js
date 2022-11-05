79ab48371686b348de0e35d99df5e9c943280092
Pedro Victor <pedro20002victor@gmail.com>
2022-11-05 11:06:18 -0300

    GRAPHQL

--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
-const { ApolloServer, gql } = require('apollo-server');
+import startServer from "./startServer";
+import typeDefs from "./graphql/typeDefs";
+import resolvers from "./graphql/resolvers";
 
-// Toda request eh post
-// Toda request bate no mesmo endpoint (/graphql)
-
-
-// Sao nomes reservados do GRAPHQL
-// Query -> Obter informacoes (GET) 
-// Mutations -> Manipular dados (POST/PUT/PATCH/DELETE)
-// Scalar Types -> String, Int, Boolean, Float e ID
-
-const typeDefs = gql`
-   type Query {
-      hello: String
-   }
-`;
-
-// Function exec here
-const resolvers = {
-   Query: {
-      hello: () => {
-         return 'Hello world!'
-      }
-   }
-};
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen()
-   .then(({ url }) => console.log(`Server started at ${url}`));
+startServer({ typeDefs, resolvers });
