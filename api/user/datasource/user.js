c55a2a1b78ce895d0d7a46ab4a0d671c2d74ff08
Pedro Victor <pedro20002victor@gmail.com>
2022-11-10 22:10:22 -0300

    addUsers error

--- api/user/datasource/user.js
+++ api/user/datasource/user.js
@@ -1,31 +1,40 @@
-const { RESTDataSource } = require('apollo-datasource-rest')
+const { RESTDataSource } = require("apollo-datasource-rest");
 
 class UsersAPI extends RESTDataSource {
-   constructor() {
-      super()
-      this.baseURL = 'http://localhost:3000'
-   }
+  constructor() {
+    super();
+    this.baseURL = "http://localhost:3000";
+  }
 
-   async getUsers() {
-      const users = await this.get('/users')
-      return users.map(async user => ({
-         id: user.id,
-         name: user.name,
-         email: user.email,
-         active: user.active,
-         role: await this.get(`/roles/${user.role}`)
-      }))
-   }
+  async getUsers() {
+    const users = await this.get("/users");
+    return users.map(async (user) => ({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      active: user.active,
+      role: await this.get(`/roles/${user.role}`),
+    }));
+  }
 
-   async getUserById(id) {
-      const user = await this.get(`/users/${id}`)
-      user.role = await this.get(`/roles/${user.role}`)
-      return user;
-   }
+  async getUserById(id) {
+    const user = await this.get(`/users/${id}`);
+    user.role = await this.get(`/roles/${user.role}`);
+    return user;
+  }
 
-   
+  async adicionaUser(user) {
+    const users = await this.get("/users");
+    user.id = users.length + 1;
+    const role = await this.get(`roles?type=${user.role}`);
+    await this.post("users", { ...user, role: role[0].id });
+    return ({
+      ...user,
+      role: role[0],
+    })
+  }
 }
 
-console.log('teste');
+console.log("teste");
 
 module.exports = UsersAPI;
